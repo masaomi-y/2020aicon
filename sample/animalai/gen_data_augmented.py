a7d42b0876da6c6c9d7ca8c3ab25be7978658eda
masaomi-y <60335248+masaomi-y@users.noreply.github.com>
2020-09-28 21:20:09 +0900

    Add files via upload

---
+++ sample/animalai/gen_data_augmented.py
@@ -0,0 +1,78 @@
+from PIL import Image, ImageEnhance
+import os, glob
+import numpy as np
+from sklearn import model_selection
+#from sklearn import cross_validation
+
+classes = ["monkey", "boar", "crow"]
+num_classes = len(classes)
+image_size = 50
+num_testdata = 100
+
+#画像の読み込み
+
+X_train = []
+X_test = []
+Y_train = []
+Y_test = []
+
+for index, classlabel in enumerate(classes):
+    photos_dir = "./" + classlabel
+    files = glob.glob(photos_dir + "/*.jpg")
+    for i, file in enumerate(files):
+        if i >= 200: break
+        image = Image.open(file)
+        image = image.convert("RGB")
+        image = image.resize((image_size, image_size))
+        data = np.asarray(image)
+
+        if i < num_testdata:
+            X_test.append(data)
+            Y_test.append(index)
+        else:
+            for angle in range(-20,20,5):
+                # 回転
+                img_r = image.rotate(angle)
+                data = np.asarray(img_r)
+                X_train.append(data)
+                Y_train.append(index)
+
+                # 反転
+                img_trans = img_r.transpose(Image.FLIP_LEFT_RIGHT)
+                data = np.asarray(img_trans)
+                X_train.append(data)
+                Y_train.append(index)
+                # 彩度
+                img_color_n = ImageEnhance.Color(img_r).enhance(0.4)
+                data = np.asarray(img_color_n)
+                X_train.append(data)
+                Y_train.append(index)
+
+                img_color_r = ImageEnhance.Color(img_trans).enhance(0.4)
+                data = np.asarray(img_color_r)
+                X_train.append(data)
+                Y_train.append(index)
+
+                # 明度
+                img_bright_n = ImageEnhance.Brightness(img_r).enhance(0.4)
+                data = np.asarray(img_bright_n)
+                X_train.append(data)
+                Y_train.append(index)
+
+                img_bright_r = ImageEnhance.Brightness(img_trans).enhance(0.4)
+                data = np.asarray(img_bright_r)
+                X_train.append(data)
+                Y_train.append(index)
+
+
+
+# X = np.array(X)
+# Y = np.array(Y)
+X_train = np.array(X_train)
+X_test = np.array(X_test)
+y_train = np.array(Y_train)
+y_test = np.array(Y_test)
+
+# X_train, X_test, y_train, y_test = model_selection.train_test_split(X, Y)
+xy = (X_train, X_test, y_train, y_test)
+np.save("./animal_aug_enh.npy", xy)
